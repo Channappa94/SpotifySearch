8befd78d977c87d1294c79063db665e5408c759f
channappa <gadadchank@msoe.edu>
2019-11-18 10:10:42 -0600

    Initial Commit

---
+++ SpotifySearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotifySearch
+//
+//  Created by IMCS2 on 11/18/19.
+//  Copyright © 2019 com.phani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
